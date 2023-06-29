62ca26e7567a46acb3e8a621d446ecd0e5a222e9
Michael <83347281+VMihail@users.noreply.github.com>
2023-06-29 23:55:51 +0300

    Update DataBaseInfo.java

--- src/main/java/org/example/utils/DataBaseInfo.java
+++ src/main/java/org/example/utils/DataBaseInfo.java
@@ -5,8 +5,8 @@ public enum DataBaseInfo {
 
   public final String driver = "org.postgresql.Driver";
   public final String url = "jdbc:postgresql://localhost:5432/postgres";
-  public final String userName = "mihildrozdov";
-  public final String pass = "QWAszx2002";
+  public final String userName = "name";
+  public final String pass = "pass";
   public final String employeesTableName = "employees";
   public final String problemsTableName = "problems";
 }
